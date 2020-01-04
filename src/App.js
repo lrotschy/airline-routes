1113370a4ca224d8e94c484e244843516dc09e90
Liela Rotschy <lielarotschy@gmail.com>
2020-01-04 16:44:13 -0500

    Update through stp 9

--- src/App.js
+++ src/App.js
@@ -24,9 +24,40 @@ class App extends Component {
     }
   }
 
+  clearFilters = (e) => {
+    e.preventDefault();
+    this.setState(this.defaultState)
+  }
+
+  selectAirline = (value) => {
+    this.setState({airline: Number(value)})
+  }
+
+  selectAirport = (value) => {
+    this.setState({airport: value})
+  }
+
+  // airports: {"code":"BGG","name":"Bingöl Airport","lat":38.861111,"long":40.5925},
+  // routes: {"airline":4533,"src":"HBE","dest":"RUH"},
+  // airlines:     {"id":2143,"name":"Egyptair"},
+
+  hasAirline = (route) => {
+    return route.airline === this.state.airline || this.state.airline === 'All';
+  }
+
+  hasAirport = (route) => {
+    return route.src === this.state.airport || route.dest === this.state.airport || this.state.airport === 'All';
+  }
+
+  filterRoutes = () => {
+    return DATA.routes.filter(route => {
+      return this.hasAirline(route) && this.hasAirport(route);
+    })
+  }
+
 
   render() {
-    const routes = DATA.routes;
+    const routes = this.filterRoutes();
 
     const columns = [
       {name: 'Airline', property: 'airline'},
@@ -43,13 +74,22 @@ class App extends Component {
           <Select
             options={DATA.airlines}
             keyName="id"
+            titleKey="All Airlines"
+            value=""
+            onSelect={this.selectAirline}
           />
         </div>
         <div className="select airport">
           <Select
             options={DATA.airports}
             keyName="code"
+            titleKey="All Airports"
+            value=""
+            onSelect={this.selectAirport}
           />
+        </div>
+        <div className="clearFilters">
+          <button className="clearFiltersButton" onClick={this.clearFilters}>Clear Filters</button>
         </div>
         <div className="table">
           <Table className="routes-table" columns={columns} rows={routes} format={this.formatValue}/>
--- src/components/select.js
+++ src/components/select.js
@@ -4,14 +4,22 @@ class Select extends Component {
 
 // props Airlines
   // airlines:     {"id":2143,"name":"Egyptair"},
-
+  handleChange = (e) => {
+    e.preventDefault();
+    this.props.onSelect(e.target.value)
+  }
 
   render() {
+
     const options = this.props.options.map((option) => {
-      return <option key={this.props.keyName}>{option.name}</option>;
+      const value = option[this.props.keyName]
+      return <option key={value} value={value}>{option.name}</option>;
     })
+
+    options.unshift(<option key={this.props.titleKey} value="All">{this.props.titleKey}</option>)
+
     return (
-      <select>
+      <select value={this.props.value} onChange={this.handleChange}>
         {options}
       </select>
     );
